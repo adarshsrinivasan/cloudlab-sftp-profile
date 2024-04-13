30cf213383c1fe4852bbbe76acc5a5886527aec4
Adarsh Srinivasan <55426913+adarshsrinivasan@users.noreply.github.com>
2024-04-13 14:51:24 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-printf "Startup script - $1 \n"
+printf "Startup script - $1 $2 \n"
 
 export SFTP_USER=$1
+export SFTP_USER_PWD=$2
 
 sudo apt install ssh
+
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 sudo tee -a /etc/ssh/sshd_config > /dev/null <<EOT
 
@@ -20,9 +23,9 @@ sudo systemctl restart sshd
 
 sudo groupadd $SFTP_USER
 
-# sudo useradd -G $SFTP_USER -d /srv/$SFTP_USER -s /sbin/nologin $SFTP_USER
+sudo useradd -G $SFTP_USER -d /srv/$SFTP_USER -s /sbin/nologin $SFTP_USER
 
-# sudo passwd $SFTP_USER
+sudo passwd $SFTP_USER_PWD
 
 sudo mkdir -p /srv/$SFTP_USER
 
